4fef5a9c28ecbb3f323960bb75eebe7b73a42cb4
noahcoolboy <46800081+noahcoolboy@users.noreply.github.com>
2023-02-22 18:59:00 +0100

    Add Kalman filter (Experimental!)

--- utils/filters.py
+++ utils/filters.py
@@ -14,6 +14,8 @@ def get_filter(settings, fps, d):
                              settings.get("beta", 80.0),
                              settings.get("dcutoff", 1.0),
                              d)
+    elif settings.get("type").lower() == 'kalman':
+        return KalmanFilter(fps, settings.get("Q", 0.1), settings.get("R", 1), d)
     else:
         raise ValueError('Unknown filter type: {}'.format(settings.get("type")))
 
@@ -68,3 +70,61 @@ class OneEuroFilter:
         RC = 1.0 / (2 * np.pi * cutoff)
         alpha = dt / (dt + RC)
         return x_prev + alpha * (x - x_prev)
+
+
+
+class KalmanFilter:
+    def __init__(self, fps, Q, R, d=2):
+        """
+        Initialize Kalman filter object.
+
+        Parameters:
+        - F: state transition matrix
+        - H: measurement matrix
+        - Q: process noise covariance matrix
+        - R: measurement noise covariance matrix
+        - x0: initial state estimate
+        - P0: initial error covariance matrix
+        """
+        self.d = d
+        self.F = np.eye(d * 2)
+        for i in range(d):
+            self.F[i, i + d] = 1.0 / fps
+        
+        self.H = np.eye(d * 2)[:d]
+        self.Q = Q * np.eye(d * 2)
+        self.R = R * np.eye(d)
+        self.x = np.zeros(d * 2)
+        self.P = np.eye(d * 2)
+
+    def predict(self):
+        """
+        Predict next state and error covariance matrix.
+        """
+        self.x = np.dot(self.F, self.x)
+        self.P = np.dot(np.dot(self.F, self.P), self.F.T) + self.Q
+
+    def update(self, z):
+        """
+        Update state estimate and error covariance matrix with measurement.
+        """
+        y = z - np.dot(self.H, self.x)
+        S = np.dot(np.dot(self.H, self.P), self.H.T) + self.R
+        K = np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))
+        self.x = self.x + np.dot(K, y)
+        I = np.eye(self.F.shape[0])
+        self.P = np.dot(np.dot(I - np.dot(K, self.H), self.P), (I - np.dot(K, self.H)).T) + np.dot(np.dot(K, self.R), K.T)
+
+    def filter(self, point, timestamp=None):
+        # Predict the next state estimate based on the previous estimate
+        x_pred = self.F @ self.x
+        P_pred = self.F @ self.P @ self.F.T + self.Q
+
+        # Compute the Kalman gain
+        K = P_pred @ self.H.T @ np.linalg.inv(self.H @ P_pred @ self.H.T + self.R)
+
+        # Update the state estimate with the new measurement
+        self.x = x_pred + K @ (point - self.H @ x_pred)
+        self.P = (np.eye(len(self.x)) - K @ self.H) @ P_pred
+
+        return self.x[:self.d]
